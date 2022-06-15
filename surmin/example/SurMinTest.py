087d5b953367eab60f3ad4063f39b11a82b41f3b
andrei <andrei.polunovskii@exness.com>
2022-06-15 13:08:33 +0300

    update autoformat

--- surmin/example/SurMinTest.py
+++ surmin/example/SurMinTest.py
@@ -2,11 +2,7 @@ import surmin.example.SurMin as sm
 
 
 def test_solve():
-    def get_sigCV( Penal, iter ):
-        return (Penal[0]-1)**2
+    def get_sigCV(Penal, iter):
+        return (Penal[0] - 1)**2
 
-    sm.SurMin ( 30, [0.0001], 1e-5, [0.5], get_sigCV )
-
-
-
-
+    sm.SurMin(30, [0.0001], 1e-5, [0.5], get_sigCV)
